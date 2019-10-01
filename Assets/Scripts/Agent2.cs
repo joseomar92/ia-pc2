2abfc3d97bc613a43dd16c6fc168acc3535a0300
carajo <joseomar9219@gmail.com>
2019-09-30 21:36:12 -0500

    ayuda

---
+++ Assets/Scripts/Agent2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Agent2 : SBAgent
+{
+	public Transform target;
+
+	void Start()
+	{
+		maxSpeed = 10f;
+		maxSteer = 0.5f;
+	}
+
+	void Update()
+	{
+		velocity += SteeringBehaviours.Arrive(this, target, 3);
+		velocity += SteeringBehaviours.Separate(this, GameManager.agents, 2f);
+		transform.position += velocity * Time.deltaTime;
+
+        if (transform.position.y < -4 || transform.position.y > 4)  {
+			velocity.y *= -1;
+        }
+
+        if (transform.position.x < -4 || transform.position.x > 4) {
+            velocity.x *= -1;
+        }
+	}
+}
